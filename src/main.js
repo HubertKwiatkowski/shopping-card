78fa4fe4848d9fe074d67fc6cd530691472e7641
warlock <h.kwiatkowski@hotmail.com>
2022-07-30 15:27:08 +0200

    added local storage

--- src/main.js
+++ src/main.js
@@ -30,12 +30,13 @@ let shopItemsData = [
   },
 ]
 
-let basket = []
+let basket = JSON.parse(localStorage.getItem('data')) || []
 
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let {id, name, price, desc, img} = x
+      let search = basket.find((x) => x.id === id) || []
       return `
       <div id="product-id-${id}" class="item">
         <img width="220" src=${img} alt="">
@@ -46,7 +47,9 @@ let generateShop = () => {
               <h2>$ ${price}</h2>
               <div class="buttons">
                 <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                <div id="${id}" class="quantity">0</div>
+                <div id="${id}" class="quantity">
+                  ${search.item === undefined ? 0 : search.item}
+                </div>
                 <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
               </div>
             </div>
@@ -69,7 +72,7 @@ let increment = (id) => {
   } else {
     search.item += 1
   }
-  // console.log(basket)
+  localStorage.setItem("data", JSON.stringify(basket))
   update(id)
 }
 
@@ -81,7 +84,7 @@ let decrement = (id) => {
   else {
     search.item -= 1
   }
-  // console.log(basket)
+  localStorage.setItem("data", JSON.stringify(basket))
   update(id)
 }
 
@@ -96,3 +99,5 @@ let calculation = () => {
   let cartIcon = document.getElementById('cartAmount')
   cartIcon.innerHTML = (basket.map((x) => x.item).reduce((x,y)=> x+y, 0))
 }
+
+calculation()
